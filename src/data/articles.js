df4ce665f919db5c644cd0577f44e0f850ade261
Jwz-git <2653667536@qq.com>
2026-02-26 00:30:56 +0800

    add friendlink && debug

--- src/data/articles.js
+++ src/data/articles.js
@@ -4,48 +4,48 @@ const articles = [
     title: '一些想法',
     tags: ['思考', '碎碎念'],
     date: '2025-07-20',
-    image: '/image/1.jpg',
-    mdPath: '/md/一些想法.md'
+    image: '/myweb/image/1.jpg',
+    mdPath: '/myweb/md/一些想法.md'
   },
   {
     id: 2,
     title: '待定的一些事',
     tags: ['待定', '长期'],
     date: '2025-07-20',
-    image: '/image/2.jpg',
-    mdPath: '/md/待定的一些事.md'
+    image: '/myweb/image/2.jpg',
+    mdPath: '/myweb/md/待定的一些事.md'
   },
   {
     id: 3,
     title: '七夕学习体验(PySide6)',
     tags: ['感想', '"无能"','碎碎念'],
     date: '2025-08-29',
-    image: '/image/3.webp',
-    mdPath: '/md/七夕学习体验(PySide6).md'
+    image: '/myweb/image/3.webp',
+    mdPath: '/myweb/md/七夕学习体验(PySide6).md'
   },
   {
     id: 4,
     title: '为什么会有短学期',
     tags: ['无语', '抱怨', '思考'],
     date: '2025-09-02',
-    image: '/image/4.jpg',
-    mdPath: '/md/为什么会有短学期.md'
+    image: '/myweb/image/4.jpg',
+    mdPath: '/myweb/md/为什么会有短学期.md'
   },
   {
     id: 5,
     title: 'CMake学习笔记',
     tags: ['学习', '笔记'],
     date: '2025-10-10',
-    image: '/image/5.png',
-    mdPath: '/md/CMake学习笔记.md'
+    image: '/myweb/image/5.png',
+    mdPath: '/myweb/md/CMake学习笔记.md'
   },
   {
     id: 6,
     title: '情人节学习总结',
     tags: ['总结', '"无能"','碎碎念'],
     date: '2026-02-17',
-    image: '/image/6.png',
-    mdPath: '/md/情人节学习总结.md'
+    image: '/myweb/image/6.png',
+    mdPath: '/myweb/md/情人节学习总结.md'
   },
 ].reverse();
 
--- src/data/friendlink.js
+++ src/data/friendlink.js
@@ -3,13 +3,19 @@ const friendLinks = [
         name: "ℱluorine's Blog",
         url: "https://lemoon1206.github.io",
         description: "技术笔记 | 学习感悟 | 心得分享",
-        avatar: "/image/ℱluorine.jpg"
+        avatar: "/myweb/image/ℱluorine.jpg"
     },
     {
         name: "Away's Blog",
         url: "https://awayc.github.io",
         description: "前端开发 | 交互设计 | 技术漫谈",
-        avatar: "/image/Away.jpg"
+        avatar: "/myweb/image/Away.jpg"
+    },
+    {
+        name: "Reyar's Blog",
+        url: "https://reyar.top/",
+        description: "music composition | 游戏开发 | 学习记录",
+        avatar: "/myweb/image/Reyar.png"
     }
 ] 
 
