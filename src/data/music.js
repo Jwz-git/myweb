f0b69b95464d41d0bf54b266d08d9c2a3914f073
Jwz-git <2653667536@qq.com>
2026-02-25 23:44:13 +0800

    debug

--- src/data/music.js
+++ src/data/music.js
@@ -5,33 +5,33 @@ const songs = ref([
         title: '发如雪',
         artist: '周杰伦',
         cover: 'https://pic.baike.soso.com/ugc/baikepic2/0/ori-20230213211625-2132552379_jpeg_606_663_40502.jpg/800',
-        url: '/music/发如雪-周杰伦.mp3'
+        url: '/myweb/music/发如雪-周杰伦.mp3'
     },
     {
         title: '蝴蝶',
         artist: '陶喆',
         cover: 'https://ts1.tc.mm.bing.net/th/id/OIP-C.LHN6bGVugZM7nklH3cNI-wHaEo?w=474&h=296&c=7&bgcl=fffffe&r=0&o=6&pid=23.1&ucfimg=1',
-        url: '/music/蝴蝶-陶喆.mp3'
+        url: '/myweb/music/蝴蝶-陶喆.mp3'
     }, 
     {
         title: 'Cruel Summer',
         artist: 'Taylor Swift',
         cover: 'https://ts1.tc.mm.bing.net/th/id/OIP-C.NW9iqW5GsPqVCKee8-BinwAAAA?w=108&h=108&c=1&bgcl=6aee15&r=0&o=7&cb=ucfimg1&dpr=1.3&pid=ImgRC&rm=3&ucfimg=1',
-        url: '/music/Cruel Summer-Taylor Swift.mp3'
+        url: '/myweb/music/Cruel Summer-Taylor Swift.mp3'
     },
     {
         title: '愿与愁',
         artist: '林俊杰',
         cover: 'https://ts4.tc.mm.bing.net/th/id/OIP-C.6KMeiNw5BUEXi6cAFWN2DwHaHa?r=0&cb=thfc1ucfi'+
         'mg=1&rs=1&pid=ImgDetMain&o=7&rm=3',
-        url: '/music/愿与愁-林俊杰.mp3'
+        url: '/myweb/music/愿与愁-林俊杰.mp3'
     },
     {
         title: '乌鸦',
         artist: '许嵩',
         cover: 'https://ts2.tc.mm.bing.net/th/id/OIP-C.zG4CKdHHG0RpCCxdoje66gHaHa?r=0&cb=thfc1ucfi'+
         'mg=1&rs=1&pid=ImgDetMain&o=7&rm=3',
-        url: '/music/乌鸦-许嵩.mp3'
+        url: '/myweb/music/乌鸦-许嵩.mp3'
     },
     {
         title: '改变自己',
@@ -39,20 +39,20 @@ const songs = ref([
         cover: 'https://ts1.tc.mm.bing.net/th/id/R-C.5366514c2d60601a5fc7afd965ce2e38?rik=BNC7Co'+
         'P37X9WpA&riu=http%3a%2f%2fp2.music.126.net%2flHab-TuxFpGuCCv661i7eg%3d%3d%2f109951163095170'+
         '102.jpg&ehk=o7%2b%2bi23C4VfyGAuDfD95MP2RsI%2fzR8%2bm9%2fH1qSDvIL0%3d&risl=&pid=ImgRaw&r=0',
-        url: '/music/改变自己-王力宏.mp3'
+        url: '/myweb/music/改变自己-王力宏.mp3'
     },
     {
         title: '春风吹',
         artist: '方大同',
         cover: 'https://ts1.tc.mm.bing.net/th/id/OIP-C.wTH5phSDHyTNq51DwS7JKwHaHa?r=0&cb=thfc1ucfi'+
         'mg=1&rs=1&pid=ImgDetMain&o=7&rm=3',
-        url: '/music/春风吹-方大同.mp3'
+        url: '/myweb/music/春风吹-方大同.mp3'
     },
     {
         title: '孤高曼波',
         artist: '',
         cover: 'https://p2.music.126.net/z150EpOTNipDBOMNnAJr6A==/109951169783176506.jpg',
-        url: '/music/孤高曼波.mp3'
+        url: '/myweb/music/孤高曼波.mp3'
     }
 ]);
 
